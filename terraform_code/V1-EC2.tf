ebd965fa338c4203124b1fa6401e623e99f76ae0
Dimple Charde <dimple2312@gmail.com>
2024-02-11 12:02:31 +0530

    commiting v1-ec2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,37 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAUQHRK7M74WH3XBSQ"
+  secret_key = "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"
 }
 
 resource "aws_instance" "demo-server" {
     ami = "ami-022e1a32d3f742bd8"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "Devops_project_key"
+    security_groups = [ "demo-sg" ]
+}
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
 }
